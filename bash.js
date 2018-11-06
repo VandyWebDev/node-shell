08655ad96f56676ce9f02a35dcb3bf63e2afc9cc
Sara Yoon <sarayoon12@gmail.com>
2018-11-06 15:17:38 -0500

    please let this work

--- bash.js
+++ bash.js
@@ -2,9 +2,10 @@
 process.stdout.write('sara >');
 
 // user event fires
-process.stdin.on('data', (data) => {
+process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-
-  process.stdout.write('Is this what you said: ' + cmd);
+  let pwd = require('./pwd');
+  process.stdout.write(pwd());
+  //process.stdout.write('Is this what you said: ' + cmd);
   process.stdout.write('\nsara >');
 });
