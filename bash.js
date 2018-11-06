c6a9b3b509a475c72761781be1eeb7a558c4b531
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2018-11-06 15:31:11 -0500

    ls module

--- bash.js
+++ bash.js
@@ -6,6 +6,10 @@ process.stdin.on('data', data => {
   const cmd = data.toString().trim();
   let pwd = require('./pwd');
   process.stdout.write(pwd());
+  if (cmd === 'ls') {
+    let ls = require('./ls');
+    ls();
+  }
   //process.stdout.write('Is this what you said: ' + cmd);
   process.stdout.write('\nsara >');
 });
